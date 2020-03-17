2351c2f5978529fed00414eb86e1ba39ae82b27a
Hanna <[email address]>
2020-03-17 15:55:40 +0100

    Första commit

---
+++ config/config.js
@@ -0,0 +1,6 @@
+const config = {
+    databaseURL: "mongodb+srv://HannaGoldhammer:TheWalkingDead09@cluster0-rsihy.mongodb.net/logintest?retryWrites=true&w=majority",
+    mail: "SG.1A7Hu7byQPG-60tDLmg8OQ.2Oc2K1WzdN0K-WbNvN7WciNO1b1q1JMwWu7GhOk7h84"
+}
+
+module.exports = config;
